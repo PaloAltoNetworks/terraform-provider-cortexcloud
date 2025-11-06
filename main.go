8c6c65343f410b36515685307245ddab6ba60ab9
Max Boynton <mboynton@paloaltonetworks.com>
2025-11-05 18:11:43 -0600

    fix: updated outposts models to use new constructors for SDK request structs + go fmt pass

--- main.go
+++ main.go
@@ -22,14 +22,7 @@ var (
 )
 
 func logBuildInfo() {
-	log.Printf("{ " + 
-		"\"GitCommit\": \"%s\"" +
-		", \"CortexServerVersion\": \"%s\"" +
-		", \"CortexServerVersion\": \"%s\"" +
-		", \"GoVersion\": \"%s\"" +
-		", \"BuildDate\": \"%s\"" +
-		"}", 
-		GitCommit, CortexServerVersion, CortexPAPIVersion, GoVersion, BuildDate)
+	log.Printf("{ \"GitCommit\": \"%s\", \"CortexServerVersion\": \"%s\", \"CortexServerVersion\": \"%s\", \"GoVersion\": \"%s\", \"BuildDate\": \"%s\"}", GitCommit, CortexServerVersion, CortexPAPIVersion, GoVersion, BuildDate)
 }
 
 func main() {
