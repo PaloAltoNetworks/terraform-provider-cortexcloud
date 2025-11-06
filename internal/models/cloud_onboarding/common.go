4bc3632de05e722ca2960702f4afd8ebcfd4e978
Max Boynton <mboynton@paloaltonetworks.com>
2025-11-06 10:31:11 -0600

    chore: remove old template resource/model and clean out unused structs in cloudonboarding

--- internal/models/cloud_onboarding/common.go
+++ internal/models/cloud_onboarding/common.go
@@ -4,40 +4,8 @@
 package models
 
 import (
-	cloudOnboardingTypes "github.com/PaloAltoNetworks/cortex-cloud-go/types/cloudonboarding"
+	//"github.com/hashicorp/terraform-plugin-framework/types"
 )
-
-type scopeModificationRegions struct {
-	Enabled bool     `json:"enabled" tfsdk:"enabled"`
-	Type    *string   `json:"type,omitempty" tfsdk:"type"`
-	Regions *[]string `json:"regions,omitempty" tfsdk:"regions"`
-}
-
-var managedByPANWTag = cloudOnboardingTypes.Tag{
-	Key: "managed_by",
-	Value: "paloaltonetworks",
-}
-
-type CollectionConfigurationModel struct {
-	AuditLogs AuditLogsModel `tfsdk:"audit_logs"`
-}
-
-type AuditLogsModel struct {
-	Enabled types.Bool `tfsdk:"enabled"`
-}
-
-type TagModel struct {
-	Key   types.String `tfsdk:"key"`
-	Value types.String `tfsdk:"value"`
-}
-
-type ScopeModificationsModel struct {
-	Regions ScopeModificationsRegionsModel `tfsdk:"regions"`
-}
-
-type ScopeModificationsRegionsModel struct {
-	Enabled types.Bool `tfsdk:"enabled"`
-}
 
 type scopeModificationRegions struct {
 	Enabled bool     `json:"enabled" tfsdk:"enabled"`
